ebb10de1498f5497770728418fb4ccc9c113bc3a
buks <ybubaki@gmail.com>
2025-05-24 23:27:48 +0000

    doc: added documentation for create issue function.

--- controllers/issue.controller.js
+++ controllers/issue.controller.js
@@ -2,6 +2,22 @@ const { issueClassifier } = require("../utils/ai");
 const db = require("../database");
 const { issueTable } = require("../database/schema");
 
+/**
+ * Creates a new issue in the database.
+ *
+ * @function createIssue
+ * @param {Express.Request} req - The Express request object.
+ * @param {Express.Response} res - The Express response object.
+ *
+ * @returns {Promise<Express.Response>} The response object.
+ *
+ * @throws {Error} If there's an error while creating the issue.
+ *
+ * @example
+ * const { createIssue } = require("../controllers/issue.controller");
+ *
+ * createIssue(req, res);
+ */
 const createIssue = async (req, res) => {
   const { description, city } = req.body;
 
@@ -11,7 +27,7 @@ const createIssue = async (req, res) => {
       message: "Internal error",
     });
   }
-  console.log(req.file.filename);
+
   try {
     const classification = await issueClassifier(description);
 
